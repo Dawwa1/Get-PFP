c06b6ef7acc44a6a79b3dcec84e6ada21f41858b
Dawwa1 <71022309+Dawwa1@users.noreply.github.com>
2021-06-22 04:14:47 -0500

    Add files via upload

--- getpfp.py
+++ getpfp.py
@@ -18,7 +18,7 @@ async def getpfp(ctx, member: discord.Member = None):
             member = ctx.author
             embed=discord.Embed()
             embed.set_image(url=member.avatar_url)
-            embed.set_footer("Developed by Dawwa#5056 - https://bit.ly/35FQ0nS")
+            embed.set_footer("Developed by Dawwa#5056")
             await ctx.send(embed=embed)
         elif member:
             embed=discord.Embed()
